3f4482d09df82af61d385fd450b2087163af7328
Grzegorz Błaszczyk <grzegorz.blaszczyk@goodylabs.com>
2014-11-24 16:52:55 +0100

    ADD: generate_key_and_csr.sh for SSL

---
+++ bash/generate_key_and_csr.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+DOMAIN=""
+ 
+if [ "x$1" == "x" ]; then
+  DOMAIN="example.com"
+else
+  DOMAIN="$1"
+fi
+     
+cd $HOME
+mkdir -p $HOME/ssl
+cd ssl
+openssl genrsa -out ${DOMAIN}.key 2048
+openssl req -new -key ${DOMAIN}.key -out ${DOMAIN}.csr
+chmod 400 *.key
+openssl req -noout -text -in ${DOMAIN}.csr
