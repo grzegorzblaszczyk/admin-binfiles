ccb2ff01bb272a9d0de2f836d5e6fd894477369e
Grzegorz Błaszczyk <grzegorz.blaszczyk@goodylabs.com>
2015-03-20 22:56:37 +0100

    CHG: longer key

--- bash/generate_key_and_csr.sh
+++ bash/generate_key_and_csr.sh
@@ -10,7 +10,7 @@ fi
 cd $HOME
 mkdir -p $HOME/ssl
 cd ssl
-openssl genrsa -out ${DOMAIN}.key 2048
+openssl genrsa -out ${DOMAIN}.key 4096
 openssl req -new -key ${DOMAIN}.key -out ${DOMAIN}.csr
 chmod 400 *.key
 openssl req -noout -text -in ${DOMAIN}.csr
