6b392d0b033be34013d9bd4bc2f96654f00236a4
Grzegorz Błaszczyk <grzegorz.blaszczyk@goodylabs.com>
2015-11-11 16:13:33 +0100

    ADD: hints for concatenating final cert file

--- bash/generate_key_and_csr.sh
+++ bash/generate_key_and_csr.sh
@@ -8,6 +8,13 @@
 
 CHMOD=`which chmod`
 OPENSSL=`which openssl`
+WGET=`which wget`
+
+INTERMEDIATE_CERT_URL="https://www.startssl.com/certs/sub.class1.server.ca.pem"
+ROOT_CERT_URL="https://www.startssl.com/certs/ca.pem"
+
+INTERMEDIATE_CERT_FILE="startssl_intermediate.crt"
+ROOT_CA_CERT_FILE="startssl_root_ca.crt"
 
 # === DO NOT EDIT BELOW THIS LINE ===
 
@@ -23,3 +30,22 @@ $OPENSSL genrsa -out ${DOMAIN}.key 4096
 $OPENSSL req -new -key ${DOMAIN}.key -out ${DOMAIN}.csr
 $CHMOD 400 *.key
 $OPENSSL req -noout -text -in ${DOMAIN}.csr
+
+echo ""
+echo "============================================================================================================================================================================================================="
+echo "    Fetching Intermediate certificate and Root CA from ${INTERMEDIATE_CERT_URL} , ${ROOT_CERT_URL} ..."
+echo "============================================================================================================================================================================================================="
+echo ""
+
+$WGET -O ${INTERMEDIATE_CERT_FILE} ${INTERMEDIATE_CERT_URL}
+$WGET -O ${ROOT_CA_CERT_FILE} ${ROOT_CERT_URL}
+
+echo ""
+echo "   Hint: "
+echo ""
+echo "Now you have to concatenate the certificate with intermediate certificate and Root CA:"
+echo ""
+echo "cat ${DOMAIN}-vanilla.crt >> $DOMAIN.crt"
+echo "cat ${INTERMEDIATE_CERT_FILE} >> $DOMAIN.crt"
+echo "cat ${ROOT_CA_CERT_FILE} >> $DOMAIN.crt"
+echo ""
